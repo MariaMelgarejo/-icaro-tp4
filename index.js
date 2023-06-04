1a9b0ff952d9910099602e24299709fa8f50d31e
María Elena Melgarejo <melgarejo_maria@hotmail.com>
2023-06-04 16:37:11 -0300

    Inicio de Tp 4

---
+++ index.js
@@ -0,0 +1,22 @@
+//ejercicio 1
+//A)escribir una funcion a la que le pase el nombre cadena.
+//B)mostrar por pantalla el saludo ¡hola nombre!.
+//
+//ejercicio 2
+//A)pedir al usuario que ingrese su nombre apellido y edad.
+//B)luego mostrar un objeto con esos datos.
+//Ej: 
+//{
+    //nombre: "",
+    //apellido: "",
+    //edad: 0
+//  }
+
+//ejercicio 3
+//A)escribir una función que espere dos parámetros.
+//B)uno debe ser un array y el otro el elemento a buscar el array.
+//C)si encuentra ese valor deberá decir  "Elemento encontrado en la posicion (posición)".
+//
+//ejercicio 4
+//A)partiemdo de una lista o array.
+//B)invertir sus elementos. ejemplo lista (auto, casa, cama) tiene que salir (cama, casa, auto).
